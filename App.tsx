7992d8118ab277a37809cd03c9ed568f5feb2fb1
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-16 17:37:37 -0300

    feat: using PollingMode to update flags automatically

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import Home from "./src/screens/Home";
-import { ConfigCatProvider } from "configcat-react";
+import { ConfigCatProvider, PollingMode } from "configcat-react";
 import { FEATURE_FLAG_SDK_KEY } from "@env";
 
 function App() {
   return (
-    <ConfigCatProvider sdkKey={`${FEATURE_FLAG_SDK_KEY}`}>
+    <ConfigCatProvider
+      sdkKey={`${FEATURE_FLAG_SDK_KEY}`}
+      pollingMode={PollingMode.AutoPoll}
+      options={{ pollIntervalSeconds: 10 }}
+    >
       <Home />
     </ConfigCatProvider>
   );
