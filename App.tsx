6bcb3211c0070216c4d317f1a2b677cf554cdd59
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-16 16:31:44 -0300

    feat: app base created

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
