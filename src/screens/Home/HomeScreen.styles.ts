e031111b36040d73d60ac66e08ca270e16246657
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-16 17:33:24 -0300

    feat: using feature flag

--- src/screens/Home/HomeScreen.styles.ts
+++ src/screens/Home/HomeScreen.styles.ts
@@ -6,9 +6,17 @@ export default StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 32,
   },
   title: {
     fontWeight: "bold",
     fontSize: 22,
+  },
+  banner: {
+    width: "100%",
+    height: 250,
+    backgroundColor: "#2e2e2e",
+    marginTop: 22,
+    borderRadius: 7,
   },
 });
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -2,11 +2,26 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import styles from "./HomeScreen.styles";
+import { useFeatureFlag } from "configcat-react";
 
 function Home() {
+  const { value: showEventFeatureBanner, loading } = useFeatureFlag(
+    "event_feature_banner",
+    false
+  );
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Carregando . . .</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Feature flags</Text>
+      {showEventFeatureBanner && <View style={styles.banner} />}
     </View>
   );
 }
